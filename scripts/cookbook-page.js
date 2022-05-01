e107638d92b7ba7d5bac652c295a4b4c0f03c834
Sarah Wang <srarwh18@live.unc.edu>
2022-05-02 03:30:29 +0900

    added change pasta picture

--- scripts/cookbook-page.js
+++ scripts/cookbook-page.js
@@ -1,13 +1,23 @@
 function editRecord() {
     document.getElementById("pastaNotes").style.visibility = 'visible';
     document.getElementById('stopEdit').style.visibility = 'visible';
+    document.getElementById('editPhoto').style.visibility = 'visible';
     document.getElementById('pastaNoteText').style.visibility = 'hidden';
     document.getElementById('editRecord').style.visibility = 'hidden';
 }
 
 function stopEdit() {
-    document.getElementById("pastaNotes", "stopEdit").style.visibility = 'hidden';
+    document.getElementById("pastaNotes").style.visibility = 'hidden';
     document.getElementById("stopEdit").style.visibility = 'hidden';
+    document.getElementById('editPhoto').style.visibility = 'hidden';
     document.getElementById("pastaNoteText").style.visibility = 'visible';
     document.getElementById('editRecord').style.visibility = 'visible';
+}
+
+function editPhoto() {
+    document.getElementById('pastaPicture').style.visibility = 'visible';
+}
+
+function closeSetting() {
+    document.getElementById('pastaPicture').style.visibility = 'hidden';
 }
